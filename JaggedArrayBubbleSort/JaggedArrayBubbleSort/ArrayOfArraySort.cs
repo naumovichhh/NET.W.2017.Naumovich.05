5e1709b0de2bce3ca2ed7b5b9f72a7188d252ab4
naumovichhh <32872842+naumovichhh@users.noreply.github.com>
2017-11-01 04:57:46 +0200

    Add files via upload

--- JaggedArrayBubbleSort/JaggedArrayBubbleSort/ArrayOfArraySort.cs
+++ JaggedArrayBubbleSort/JaggedArrayBubbleSort/ArrayOfArraySort.cs
@@ -8,8 +8,7 @@ namespace Day5
 {
     public static class ArrayOfArraySort
     {
-        public static void Sort(int[][] array, SortMethod sortMethod = SortMethod.BySum,
-            SortOrder sortOrder = SortOrder.Ascending)
+        public static void Sort(int[][] array, IComparer<int[]> comparer, SortOrder sortOrder = SortOrder.Ascending)
         {
             if (array == null)
                 throw new ArgumentNullException("array", "Null Reference");
@@ -18,7 +17,7 @@ namespace Day5
             {
                 for (int i = 0; i < border - 1; ++i)
                 {
-                    if (NeedSwap(array[i], array[i + 1], sortMethod, sortOrder))
+                    if (NeedSwap(array[i], array[i + 1], comparer, sortOrder))
                     {
                         Swap(ref array[i], ref array[i + 1]);
                         tempBorder = i + 1;
@@ -29,62 +28,20 @@ namespace Day5
             }
         }
 
-        private static bool NeedSwap(int[] low, int[] high, SortMethod method, SortOrder order)
+        private static bool NeedSwap(int[] low, int[] high, IComparer<int[]> comparer, SortOrder order)
         {
             if (order == SortOrder.Ascending)
-                if (Compare(low, high, method) > 0)
+                if (comparer.Compare(low, high) > 0)
                     return true;
                 else
                     return false;
             else if (order == SortOrder.Descending)
-                if (Compare(low, high, method) < 0)
+                if (comparer.Compare(low, high) < 0)
                     return true;
                 else
                     return false;
             else
                 throw new ArgumentException("Argument \"order\" should be of SortOrder values");
-        }
-
-        private static int Compare(int[] a, int [] b, SortMethod method)
-        {
-            if (a == null)
-                if (b == null)
-                    return 0;
-                else
-                    return -1;
-            else if (b == null)
-                return 1;
-
-            switch (method)
-            {
-                case SortMethod.BySum:
-                    int sumA = 0, sumB = 0;
-                    for (int i = 0; i < a.Length; ++i)
-                        sumA += a[i];
-                    for (int i = 0; i < b.Length; ++i)
-                        sumB += b[i];
-                    return sumA - sumB;
-                case SortMethod.ByMax:
-                    int maxA = int.MinValue, maxB = int.MinValue;
-                    for (int i = 0; i < a.Length; ++i)
-                        if (a[i] > maxA)
-                            maxA = a[i];
-                    for (int i = 0; i < b.Length; ++i)
-                        if (b[i] > maxB)
-                            maxB = b[i];
-                    return maxA - maxB;
-                case SortMethod.ByMin:
-                    int minA = int.MaxValue, minB = int.MaxValue;
-                    for (int i = 0; i < a.Length; ++i)
-                        if (a[i] < minA)
-                            minA = a[i];
-                    for (int i = 0; i < b.Length; ++i)
-                        if (b[i] < minB)
-                            minB = b[i];
-                    return minA - minB;
-                default:
-                    throw new ArgumentException("Argument \"method\" should be of SortMethod values");
-            }
         }
 
         private static void Swap(ref int[] a, ref int[] b)
@@ -94,7 +51,6 @@ namespace Day5
             b = temp;
         }
     }
-
-    public enum SortMethod { BySum, ByMax, ByMin }
+    
     public enum SortOrder { Ascending, Descending }
 }
